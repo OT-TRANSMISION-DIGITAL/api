cefa0cff7a7a14bc6ea3068d0c68a9a991163d2c
xXMarcelaXx <110473185+xXMarcelaXx@users.noreply.github.com>
2024-06-11 18:13:51 -0600

    migrations usuarios y roles https://app.asana.com/0/1207418327256065/1207535091993120/f

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,18 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('img');
+            $table->string('nombre');
+            $table->string('correo')->unique();
+            $table->string('telefono');
+            $table->boolean('estatus');
+            $table->string('contraseña');
+            $table->string('rol_id');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreign('rol_id')->references('id')->on('roles');
         });
     }
 
